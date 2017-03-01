09daed81cf36ea14be42a4c3c54b6d90f8e8f1d2
brown4224 <swmcglincy@hotmail.com>
2017-02-28 23:20:46 -0500

    CSS for main page, main page functionallity

--- src/app/public/home/home.ts
+++ src/app/public/home/home.ts
@@ -12,74 +12,85 @@ import { UserService } from '../../services/user';
 })
 
 export class Home implements OnInit {
+
   public username: string;
-
-
-  private folders: folderInterface[];
-  //Stor IDs
-  private folder: number;
-  private file: number;
+  private working : boolean;
+  private folders: folderInterface[];  //The DATA
 
   constructor(public router: Router, private api: APIService, private user: UserService) {
-    this.folder = null;
-    this.file = null;
   }
 
-  ngOnInit() {
+  private ngOnInit() {
+    this.working = true;
     this.username = this.user.getUsername();
     // Load folder on page load
     console.log('Firing Homepage On Init');
     this.api.getALLFolders().subscribe(
       data => {
         if (data) {
-          console.log(data.status);
           this.folders = data.folders;
         }
       }, err => {
-        alert('There was a problem loading the folders.')
+        this.queryError(err);
       });
-  }
+      this.working = false;
+    }
 
-  //Load files when Folder clicked
-  loadFiles(id: number) {
-    this.folder = id;
-    this.api.getFilesWithID(id).subscribe(
-      data => {
-        if (data) {
-          console.log(data.status);
-          this.folders[id].files= data.files;
-          this.folders[id].showFiles= true;
+    private loadFiles(id: number){
+      this.working = true;
+      id--; // ofset
+      if(!this.folders[id].loaded){
+        this.queryFiles(id);
+      } else{
+        this.showToggle(id);
+      }
+      this.working = false;
+    }
 
+    //Load files when Folder clicked
+    private queryFiles(id: number) {
+      this.api.getFilesWithID(id).subscribe(
+        data => {
+          if (data) {
+            this.folders[id].files= data.files;
+            this.folders[id].show = true;
+            this.folders[id].loaded = true;
+          }
+        }, err => {
+          this.queryError(err);
+        });
+      }
+      private queryError(err: any){
+        if(!this.user.isLoggedIn()){  // Will logout automatic
+          this.router.navigate(['login']);
+          alert('Your session has expired.');
+        } else {
+          alert('There was a problem loading the folders.');
         }
-      }, err => {
-        alert('There was a problem loading the files.')
-      });
-  }
+      }
 
-  logout() {
-    this.user.unsetUser();
-    this.router.navigate(['login']);
-  }
+      private logout() {
+        this.user.unsetUser();
+        this.router.navigate(['login']);
+      }
 
-  showFiles(id: number){
-    this.folders[id].showFiles = !this.folders[id].showFiles;
-  }
+      private showToggle(id : number){
+        this.folders[id].show = !this.folders[id].show;
+      }
+    }
 
+    // Interfaces
 
-
-}
-
-// Interfaces
-
-interface folderInterface {
-  showFiles : boolean;
-  path: string;
-  id: number;
-  files: fileInterface[];
-}
-interface fileInterface {
-  id: number;
-  name: string;
-  folderID: number;
-  mime: string;
-}
+    interface folderInterface {
+      path: string;
+      id: number;
+      files: fileInterface[];
+      loaded : boolean;
+      show : boolean;
+    }
+    interface fileInterface {
+      id: number;
+      name: string;
+      folderID: number;
+      mime: string;
+    }
